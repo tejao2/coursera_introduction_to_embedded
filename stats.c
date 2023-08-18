18836f3a2a36895ea1ff3de0af10a054b1eb1b8f
tejao2 <77572640+tejao2@users.noreply.github.com>
2023-08-18 16:50:25 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -27,33 +27,6 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  unsigned char minimum = 0;
-  unsigned char maximum = 0;
-  float mean = 0;
-  unsigned char median = 0;
-
-  /* Statistics and Printing Functions Go Here */
-  printf("Array before sorting: \n");
-  print_array(&test, SIZE);
-  median = find_median(&test, SIZE);
-  mean = find_mean(&test, SIZE);
-  maximum = find_maximum(&test, SIZE);
-  minimum = find_minimum(&test, SIZE);
-  print_statistics(minimum, maximum, mean, median);
-  sort_array(&test, SIZE);
-  printf("Array after sorting: \n");
-  print_array(&test, SIZE);
-}
-
 void print_statistics (unsigned char minimum, unsigned char maximum, float mean, unsigned char median){
   printf("The minimum is %d \n", minimum);
   printf("The maximum is %d \n", maximum);
@@ -128,4 +101,31 @@ void sort_array (unsigned char *array, unsigned int counter){
     }
   }
   while (flag ==1); // the array is not sorted as long as a swap operation is occurred at least once
+}
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  unsigned char minimum = 0;
+  unsigned char maximum = 0;
+  float mean = 0;
+  unsigned char median = 0;
+
+  /* Statistics and Printing Functions Go Here */
+  printf("Array before sorting: \n");
+  print_array(&test, SIZE);
+  median = find_median(&test, SIZE);
+  mean = find_mean(&test, SIZE);
+  maximum = find_maximum(&test, SIZE);
+  minimum = find_minimum(&test, SIZE);
+  print_statistics(minimum, maximum, mean, median);
+  sort_array(&test, SIZE);
+  printf("Array after sorting: \n");
+  print_array(&test, SIZE);
 }
